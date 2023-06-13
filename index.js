e6d66aca3c6749a6b62c3b3d0dbaee7649969df1
MarzukaaZaki <zakimarzuka@gmail.com>
2023-06-14 00:05:35 +0600

    Added make instructor api

--- index.js
+++ index.js
@@ -71,8 +71,22 @@ async function run() {
       res.send(result);
     })
 
-    // Save user email and role in database
+    // Make Instructor
+    app.patch('/users/instructor/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          role: 'instructor'
+        }
+      };
 
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // Save user email and role in database
     app.put('/users/:email', async (req, res)=>{
       const email = req.params.email;
       const user = req.body;
