dd79a803efdd14ef12e564b814172f4228fd1200
MarzukaaZaki <zakimarzuka@gmail.com>
2023-06-14 23:38:49 +0600

    Delete cart api

--- index.js
+++ index.js
@@ -255,6 +255,12 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
